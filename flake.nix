e3ec6dfa198b1966b0bc0e6acb60f96bf27f9412
grigory <gr.qchinsky@yandex.ru>
2025-11-08 19:31:22 +0500

    added flake.nix sdgyrodsu.nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+
+      systems = lib.systems.flakeExposed;
+
+      forAllSystems = lib.genAttrs systems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+      });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                usbutils
+                nixpkgs-fmt
+              ];
+          };
+        });
+
+      overlays.default = final: prev: {
+        sdgyrodsu = final.callPackage ./sdgyrodsu.nix { };
+      };
+
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        lib.makeScope pkgs.newScope (final: self.overlays.default final pkgs)
+      );
+    };
+}
---
+++ sdgyrodsu.nix
@@ -0,0 +1,47 @@
+{
+  #lib,
+  stdenv,
+  fetchFromGitHub,
+  ncurses,
+  hidapi,
+  systemd,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "sdgyrodsu";
+  version = "2.1.1";
+
+  src = fetchFromGitHub {
+    owner = "kmicki";
+    repo = "SteamDeckGyroDSU";
+    tag = "v${finalAttrs.version}";
+    sha256 = "sha256-24ZnX9n4vIMFwNWbQOT7aLvlX7uiTzMVRHfuaTkwg6M=";
+  };
+
+  buildInputs = [
+    ncurses
+    hidapi
+    systemd
+  ];
+
+  makeFlags = [
+    "NOPREPARE=1"
+    "CHECKDEPS="
+    "release"
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp -r bin/release/sdgyrodsu $out/bin
+
+    runHook postInstall
+  '';
+
+#   meta = with lib; {
+#     description = "Cemuhook DSU server for the Steam Deck Gyroscope";
+#     homepage = "https://github.com/kmicki/SteamDeckGyroDSU";
+#     license = licenses.mit;
+#   };
+})
